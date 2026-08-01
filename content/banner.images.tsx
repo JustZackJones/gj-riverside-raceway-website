b867534eaf6be152e1d92472a960e9f7d77c88a2
Zack Jones <justzackj@gmail.com>
2026-07-31 21:38:38 -0600

    Update banner.images.tsx

--- content/banner.images.tsx
+++ content/banner.images.tsx
@@ -1,13 +1,6 @@
 
 export const bannerImages: BannerImage[] = [
     {
-        src: "/images/fliers/summer-spintacular-2026.jpg",
-        link: "/summer-spintacular",
-        objectPosition: "center 50%",
-        objectFit: "contain",
-        displayTimeMs: 10000,
-        active: true
-    }, {
         src: "/images/outdoor-track/3.28.2026/410-sprint-lineup.jpg",
         objectPosition: "center 50%",
         objectFit: "cover",
