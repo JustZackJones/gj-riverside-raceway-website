314fb276d6d2fc631af6e8c414feb7b6a21bef5c
Zack Jones <justzackj@gmail.com>
2026-08-08 11:08:43 -0600

    normalize livetime urls

--- content/livetime.tsx
+++ content/livetime.tsx
@@ -3,6 +3,9 @@ export const livetime = {
     trackName: "JJ's Raceway",
     eventsPath: 'events/',
     resultsPath: 'results/?p=view_event&id=',
-    getLink: (path: string) => `${livetime.baseUrl}${path}`,
+    getLink: (path: string) => {
+        const normalizedPath = path.startsWith('/') ? path.slice(1) : path;
+        return `${livetime.baseUrl}${normalizedPath}`;
+    },
     getResultLink: (eventId: number) => `${livetime.baseUrl}${livetime.resultsPath}${eventId}`,
 }
