f118a88a93a91531136483281da7526527373966
Zack Jones <justzackj@gmail.com>
2026-08-08 10:08:05 -0600

    Update facebook.tsx

--- content/facebook.tsx
+++ content/facebook.tsx
@@ -6,5 +6,13 @@ export const facebook = {
         <a href={facebook.groupUrl} target="_blank" rel="noopener noreferrer" className='text-[#1877F2]'>
             Check facebook for more info!
         </a>
+    ),
+    primaryContactUrl: 'https://www.facebook.com/groups/961239392247400/user/61551234750535',
+    primaryContactName: 'John Jones',
+    primaryContactContent: (): React.JSX.Element => (
+        <a href={facebook.primaryContactUrl} target="_blank" rel="noopener noreferrer" className='text-[#1877F2]'>
+            {facebook.primaryContactName}
+        </a>
     )
+
 }
