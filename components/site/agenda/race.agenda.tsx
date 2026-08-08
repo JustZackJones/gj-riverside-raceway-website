59f94cae9f760e69ae623096a4135716e4785282
Zack Jones <justzackj@gmail.com>
2026-08-08 10:08:11 -0600

    Update race.agenda.tsx

--- components/site/agenda/race.agenda.tsx
+++ components/site/agenda/race.agenda.tsx
@@ -3,7 +3,7 @@ import { events } from '@/content/content';
 import { Column } from '@/components/ui/ui'
 import BriefContentHeader from '@/components/site/brief/brief.content.header';
 import RaceEventAgenda from '@/components/site/agenda/race.event.agenda';
-import { SitePhoneDisplayForPractice } from '../site.phone.display';
+import { SiteFaceBookMessageDisplayForPractice } from '../site.facebook.message.display';
 
 export default function RaceAgenda({className, style, width = "350px"}: {className?: string, style?: React.CSSProperties, width?: string}) {
     return (
@@ -11,7 +11,7 @@ export default function RaceAgenda({className, style, width = "350px"}: {classNa
             <BriefContentHeader icon="fa-solid fa-clock">Agenda</BriefContentHeader>
             <RaceEventAgenda event={events.tuesday} />
             <RaceEventAgenda event={events.saturday} />
-            <SitePhoneDisplayForPractice/>
+            <SiteFaceBookMessageDisplayForPractice/>
         </Column>
     )
 }
